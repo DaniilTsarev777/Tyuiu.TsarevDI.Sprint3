a41df4433ea1211820469b36a7f7eb3540bf5fee
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-17 18:05:52 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.TsarevDI.Sprint3.Task1.V4.Lib;
+namespace Tyuiu.TsarevDI.Sprint3.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService(); 
+            int startValue = 1;
+            int stopValue = 10;
+            Assert.AreEqual(1.55, ds.GetSumSeries(startValue, stopValue));
+        }
+    }
+}
