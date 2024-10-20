e77cf53c4411bb44b8caf270d73f297aa6b97e80
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-20 17:16:57 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task2.V9.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsarevDI.Sprint3.Task2.V9.Lib
+{
+    public class DataService : ISprint3Task2V9
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            int n = 5;
+            double s = 0;
+            do
+            {
+                s += Math.Pow(1 / Math.Pow(n, startValue), 2);
+                startValue++;
+            }
+            while (startValue <= stopValue);
+            return Math.Round(s, 3);
+        }
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint3.Task2.V9/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TsarevDI.Sprint3.Task2.V9.Lib;
+namespace Tyuiu.TsarevDI.Sprint3.Task2.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #3                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                         *");
+            Console.WriteLine("* Задание #2                                                            *");
+            Console.WriteLine("* Варинат #11                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            int value, startValue, stopValue;
+            startValue = 1;
+            value = 5;
+            stopValue = 10;
+
+
+            Console.WriteLine("value = 5");
+            Console.WriteLine("startValue = 1");
+            Console.WriteLine("stopValue = 10");
+
+
+
+            
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
