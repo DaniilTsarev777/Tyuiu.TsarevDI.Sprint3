66c64413e9363ef75f75d087dd8b91279ff73285
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-22 18:29:26 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task5.V18.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsarevDI.Sprint3.Task5.V18.Lib
+{
+    public class DataService : ISprint3Task5V18
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double y;
+            y = 0;
+            for (int i = startValue1; i <= stopValue1; startValue1++)
+                for (int k = startValue2; k <= stopValue2; startValue2++)
+                    y += Math.Cos(x) + ((k * k) / 2);
+            return Math.Round(y, 3);
+
+            
+        }
+    }
+}
