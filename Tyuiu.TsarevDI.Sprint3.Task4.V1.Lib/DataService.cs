592412e040a4c5f3b95120cb888ed2a3f6071508
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-22 16:14:02 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsarevDI.Sprint3.Task4.V1.Lib
+{
+    public class DataService : ISprint3Task4V1
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double y;
+            y = 0;
+            for (startValue = -5; startValue <= stopValue; startValue++)
+                if (startValue == 0)
+                    break;
+                else
+                    y += Math.Sin(startValue) / startValue;
+
+
+
+            return Math.Round(y, 3);
+        }
+    }
+}
