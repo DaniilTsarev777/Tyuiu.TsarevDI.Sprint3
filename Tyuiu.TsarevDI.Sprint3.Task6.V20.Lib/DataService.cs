4a957648b07ba4d25e2494d625d64d184cadbd6f
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-23 18:19:43 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsarevDI.Sprint3.Task6.V20.Lib
+{
+    public class DataService : ISprint3Task6V20
+
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x = startValue;
+            int c = 0;
+            for (x = 20; x <= stopValue; x++)
+                for (int d = 1; d <= x; d++)
+                    if ((x % d == 0) && (d < 12))
+                        c++;
+            return c;
+        }
+    }
+}
