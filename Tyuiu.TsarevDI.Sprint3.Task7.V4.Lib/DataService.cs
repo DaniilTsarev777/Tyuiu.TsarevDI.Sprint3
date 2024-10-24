eea9fe3a72ce95d7858a156bebc403d5741b288a
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-10-24 19:39:04 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint3.Task7.V4.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Schema;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.TsarevDI.Sprint3.Task7.V4.Lib
+{
+    public class DataService : ISprint3Task7V4
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int count;
+
+            double[] m = new double[11];
+ 
+            count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+
+                m[count] = Math.Round((2 * x + 6) / (Math.Cos(x) + x) - 3, 2);
+                
+                count++;
+
+            return m;
+        }
+    }
+}
